bd6ed47b45970c82b1c457346167270e50e23150
AlHalimThoriqF <alhalimthof522@gmail.com>
2024-06-05 14:27:22 +0700

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Kalkulator (Tugas2)"
+include(":app")
+ 
